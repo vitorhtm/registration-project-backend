6efdfbeb439edf6a1823dffdd48548ac97be1387
Vitor Monteiro <vitor.monteiro@o2obots.com>
2025-12-17 14:52:33 -0300

    fix: melhorando validação de cpf e cnpj

--- src/registration/dto/update-document.dto.ts
+++ src/registration/dto/update-document.dto.ts
@@ -1,8 +1,11 @@
-import { IsNotEmpty, Matches } from 'class-validator';
+import { cpf, cnpj } from 'cpf-cnpj-validator';
+import { IsNotEmpty, Matches, Validate } from 'class-validator';
 
 export class UpdateDocumentDto {
   @IsNotEmpty({ message: 'CPF ou CNPJ é obrigatório' })
-  // Regex simples para aceitar números (pode melhorar depois)
   @Matches(/^\d+$/, { message: 'CPF ou CNPJ deve conter apenas números' })
+  @Validate((value: string) => cpf.isValid(value) || cnpj.isValid(value), {
+    message: 'CPF ou CNPJ inválido',
+  })
   document: string;
 }
